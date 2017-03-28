d7baa46cfb07998936cd6b7d9d08ae12afc910f5
dodoru <dodoru@foxmail.com>
2017-03-28 21:08:04 +0800

    pyco_scripts , 常用的脚本和模板文件

---
+++ pyco_scripts/__init__.py
@@ -0,0 +1,17 @@
+from manager import Manager
+# pip install manage.py
+
+manager = Manager()
+
+
+@manager.command
+def command(args, kwargs=''):
+    '''
+    eg: python manage.py command args --kwargs='xxx'
+    '''
+    print(args, type(args))
+    print(kwargs, type(kwargs))
+
+
+if __name__ == '__main__':
+    manager.main()
