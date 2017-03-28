e05f9469ce3c9509c53b5422027a54b0bc029b14
dodoru <dodoru@foxmail.com>
2017-03-28 21:18:25 +0800

    setup 打包

---
+++ setup.py
@@ -0,0 +1,99 @@
+
+"""
+pyco_utils
+======
+
+See:
+* document:
+* todo:
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+setup(
+    name='pyco_utils',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.0.1',
+
+    description='pyco_utils, ',
+    long_description=__doc__,
+
+    # The project's main homepage.
+    url='http://github.com/vmico/pyco-utils',
+    author='dodoru',
+    author_email='dodoru@foxmail.com',
+    maintainer='dodoru',
+    maintainer_email='dodoru@foxmail.com',
+
+    zip_safe=False,
+    platforms='any',
+    license='MIT',
+
+    # What does your project relate to?
+    keywords='Python utils',
+
+    install_requires=[
+    ],
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=[
+        'pyco_utils',
+    ],
+
+    include_package_data=True,
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[
+    ],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+    },
+
+)
