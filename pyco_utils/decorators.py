2819b12497c0987aa232fb7c7068cf46da0fb137
dodoru <dodoru@foxmail.com>
2017-04-02 22:38:29 +0800

    添加 ajax_func , 和 singleton

--- pyco_utils/decorators.py
+++ pyco_utils/decorators.py
@@ -1,3 +1,10 @@
+# coding: utf-8
+
+import threading
+from functools import wraps
+
+from .logger import log
+
 
 def retry(func, count=3):
     def decorator(*args, **kwargs):
@@ -5,7 +12,31 @@ def retry(func, count=3):
             try:
                 return func(*args, **kwargs)
             except Exception as e:
-                log(e.args, e.message, func, *args, level='error', **kwargs)
+                log('[retry Error]', func.__name__, *args, **kwargs)
+                log(e)
         return func(*args, **kwargs)
 
     return decorator
+
+
+def ajax_func(func, daemon=True):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        th = threading.Thread(target=func, args=args, kwargs=kwargs)
+        th.daemon = daemon
+        log(func.__name__, args, kwargs, daemon)
+        th.start()
+
+    return wrapper
+
+
+def singleton(cls):
+    # 使用这个装饰器的类，不能作为父类被继承
+    instance = {}
+
+    def get_instance(*args, **kwargs):
+        if cls not in instance:
+            instance[cls] = cls(*args, **kwargs)
+        return instance[cls]
+
+    return get_instance
